78990f44bbc97ab904b033cdac25ddbe5bfdda21
Livingston Samuel <livingston.samuel@gmail.com>
2010-05-20 17:26:51 +0530

    Rewrote the plugin from scratch, updated demo page

--- jquery.autoSize.js
+++ jquery.autoSize.js
@@ -1,101 +1,74 @@
+/*! autoSize - based on autoSize of Josh Pyles
+ *
+ * @author - Livingston Samuel, http://livingstonsamuel.com/
+ * @license - MIT License - http://www.opensource.org/licenses/mit-license.php
+ *
+ * Original Copyright © 2010 Josh Pyles / Pixelmatrix Design LLC, http://pixelmatrixdesign.com
+ */
 /*
-
-autoSize v0.1
-Copyright © 2010 Josh Pyles / Pixelmatrix Design LLC
-http://pixelmatrixdesign.com
-
-Requires jQuery 1.3 or newer
-
-License:
-MIT License - http://www.opensource.org/licenses/mit-license.php
-
-Adds customizable classes to objects based on their length 
-so you can make them the optimal size for the space available
-
-Basic Usage:
-$("#sizeme").autoSize();
-
-Custom Lengths:
-$("#sizeme").autoSize({classes: {tiny: 0, small: 100, medium: 500, large: 1000}});
-
-$("#sizeme").autoSize({classes: {d0: 0, d1: 1, d2: 2, d3: 3, d4: 4}});
-
-Parameters:
-classes (object)
-- an object of key value pairs with the class name as the key, and the value being the minimum # of characters
-
-Enjoy!
-
-*/
+ * Requires jQuery 1.3 or newer
+ *
+ * Adds customizable classes to objects based on their length
+ * so you can make them the optimal size for the space available
+ *
+ * Basic Usage:
+ *   $("#sizeme").autoSize();
+ *
+ * Custom Lengths:
+ *   $("#sizeme").autoSize({classes: {tiny: 0, small: 100, medium: 500, large: 1000}});
+ *   $("#sizeme").autoSize({classes: {d0: 0, d1: 1, d2: 2, d3: 3, d4: 4}});
+ *
+ * @params - classes (object)
+ *           an object of key value pairs with the class name as the key, and the value being the minimum # of characters
+ *
+ */
 
 (function($) {
-  $.fn.autoSize = function(options) {
-    //map vars correctly
-    
-    //debug(this);
-    // build main options before element iteration
-    var opts = $.extend({}, $.fn.autoSize.defaults, options);
-    // iterate and setup each matched element
-    return this.each(function() {
-      var $this = $(this);
-      // build element specific options
-      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-      
-      var text = $this.text();
-      
-      var length = text.length;
-      
-      var theClass;
-      
-      var classes = [];
-      
-      $.each(o.classes, function(k,v){
-        classes.push([k, v]);
-      });
-      
-      classes = classes.sort(function(a,b){
-        return a[1] - b[1];
-      });
+  $.fn.autoSize = (function () {
+    var defaults = {
+          classes:{ tiny: 0,
+                    small: 200,
+                    medium: 1000,
+                    large: 3000
+                  }
+        },
+        toArray = function (obj) {
+          var key, result = [];
 
-      var matches = [];
-      
-      for(i=0; i < classes.length; i++){
-        if(length >= classes[i][1]){
-          // if 4 >= 1
-          matches.push(classes[i]);
-        }else{
-          break;
+          for(key in obj) {
+            if(obj.hasOwnProperty(key)) {
+              result[key] = obj[key];
+            }
+          }
+
+          return result;
+        };
+
+    return function (options) {
+      var opts = $.extend({}, defaults, options); // build main options before element iteration
+
+      return this.each(function () { // iterate and setup each matched element
+        var $this = $(this),
+            o = $.meta ? $.extend({}, opts, $this.data()) : opts, // build element specific options
+            len = $this.text().length,
+            classnames = [], cls, match,
+            classes = o.classes, mapping = [], temp;
+
+        for(cls in classes) {
+          if (classes.hasOwnProperty(cls)) {
+            temp = classes[cls];
+            if(len >= temp) {
+              mapping[temp] = cls
+            }
+            classnames.push(cls);
+          }
         }
-      }
-      
-      matches = matches.sort(function(a,b){
-        return b[1] - a[1];
+
+        match = mapping.sort().shift();
+        classnames = classnames.join(' ');
+
+        $this.removeClass(classnames).addClass(match);
       });
-      
-      var classnames = [];
-      
-      $.each(o.classes, function(k,v){
-        classnames.push(k);
-      })
-      
-      classnames = classnames.join(" ");
-      
-      if(matches.length > 0){
-        theClass = matches[0][0];
-        $this.removeClass(classnames);
-        $this.addClass(theClass);
-      }
-    });
-  };
-  //
-  // plugin defaults
-  //
-  $.fn.autoSize.defaults = {
-    classes: {
-      tiny: 0,
-      small: 200,
-      medium: 1000,
-      large: 3000
-    }
-  };
-})(jQuery);
+    };
+  }());
+}(jQuery));
